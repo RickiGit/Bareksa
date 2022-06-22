9a95dbb411b1d4ea0e7e97114aecc9e02b611bb3
rickipcman <100334904+rickipcman@users.noreply.github.com>
2022-06-22 22:15:52 +0700

    add splashscreen

--- app/src/main/java/com/product/managefund/MainActivity.kt
+++ app/src/main/java/com/product/managefund/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 import com.product.managefund.View.CompareActivity
+import com.product.managefund.View.ListProductActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         // Create Loading Splashscreen
         Handler(Looper.getMainLooper()).postDelayed({
-            val main = Intent(this, CompareActivity::class.java)
+            val main = Intent(this, ListProductActivity::class.java)
             startActivity(main)
         }, 2000)
     }
